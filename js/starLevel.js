57b4b7d9b0858c12b28f9d6f2a6b42c032d5fd98
dandelion <1451172112@qq.com>
2018-03-02 15:08:59 +0800

    update script

--- js/starLevel.js
+++ js/starLevel.js
@@ -1,34 +1,46 @@
+var starImgArr = [];
+
 var starLevel = function(dom,level,option){
 	option = option ||{};
 	this.bigR = option.bigR||12;
 	this.smallR = option.smallR||5;
-	this.width = option.width||200;
-	this.height= option.height||this.bigR;
+	this.width = option.width||100;
+	this.height= option.height||this.bigR*2;
 	this.borderColor = option.borderColor||"#ffc654";
 	this.fillColor = option.fillColor||"#ffc654";
 	this.space = option.space||5;
 	this.lineWidth = option.lineWidth||1;
-
 	this.initLevel(dom,level);
 }
 starLevel.prototype = {
 	initLevel:function(dom,level){
-		let canvas = document.createElement('canvas');
-		let ctx = canvas.getContext("2d");
+		var starImg,tpl,levelImg;
 		level = parseInt(level);
-		canvas.width=this.width*2;
-		canvas.height=this.height*2
+		levelImg =starImgArr[level];
+
+		if (levelImg == ""||levelImg == undefined) {
+			this.drwaCanvs(dom,level);
+		}else{
+			var img = document.createElement('img');
+			img.src = levelImg;
+			dom.appendChild(img);
+		}
+	},
+	drwaCanvs:function(dom,level){
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext("2d");
+		canvas.width=this.width;
+		canvas.height=this.height;
 		ctx.lineCap = 'round';
 		ctx.lineJoin = 'round';
-		
-		for (let i = 0;i<=level-1;i++) {
+		for (var i = 0;i<=level-1;i++) {
 			this.drawStar(ctx,{x:this.bigR+this.bigR*2*i+i*this.space,y:this.bigR},true);
 		}
-		for (let j=level+1;j<=5;j++) {
+		for (var j=level+1;j<=5;j++) {
 			this.drawStar(ctx,{x:this.bigR+this.bigR*2*(j-1)+(j-1)*this.space,y:this.bigR},false);
 		}
-
-		ctx.scale(0.5,0.5);
+		starImg = canvas.toDataURL("image/png");
+		starImgArr[level] = starImg;
 		dom.appendChild(canvas);
 	},
 	drawStar:function(ctx,p,isFill){
@@ -36,7 +48,7 @@ starLevel.prototype = {
 	                        -Math.sin(18/180*Math.PI)*this.bigR+p.y); 
 		ctx.lineTo(Math.cos(54/180*Math.PI)*this.smallR+p.x,   
 	                        -Math.sin(54/180*Math.PI)*this.smallR+p.y); 
-		for (let i = 1; i < 5; i++) { 
+		for (var i = 1; i < 5; i++) { 
 	        ctx.lineTo(Math.cos((18+i*72)/180*Math.PI)*this.bigR+p.x,   
 	                        -Math.sin((18+i*72)/180*Math.PI)*this.bigR+p.y);   
 	        ctx.lineTo(Math.cos((54+i*72)/180*Math.PI)*this.smallR+p.x,   
